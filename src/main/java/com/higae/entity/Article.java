81315df9874c710606992652034187d638ef568d
QQAnh <thanhlm@nal.vn>
2019-05-31 09:50:33 +0700

    [Add] multiple language exceptions

---
+++ src/main/java/com/higae/entity/Article.java
@@ -0,0 +1,48 @@
+package com.higae.entity;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+@Entity
+public class Article {
+    @Id
+    private long id;
+    @Index
+    private String title;
+    @Index
+    private String content;
+
+    public Article() {
+    }
+
+    public Article(long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
